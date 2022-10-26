ad1c8236fa93deb1da1443b315a6a957f6e11f92
danna bello <dmbellom@correo.udistrital.edu.co>
2022-10-26 17:57:16 -0500

    v.0.0.2

---
+++ Sky.py
@@ -0,0 +1,23 @@
+import random
+
+class Sky:
+    
+    def __init__(self, width, height,quantity):
+        
+        self.stars= []
+        self.width= width
+        self.height= height
+        
+        for i in range(quantity):
+            
+            x= random.randint(0, width)
+            y= random.randint(0, height)
+            
+            self.stars.append([x,y])
+            
+    def move(self):
+        
+        for star in self.stars:
+            star[1]+= 1
+            if star[1]> self.height:
+                star[1]= 0
